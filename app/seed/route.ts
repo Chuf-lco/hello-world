f9958b70ed956fccdbd323d508e550e091d6b894
Chuf-lco <mainaantony07@gmail.com>
2024-07-18 18:13:26 +0300

    configuring seeded placeholder db

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,10 +102,9 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  //return Response.json({
+    //message:
+  //})
   try {
     await client.sql`BEGIN`;
     await seedUsers();
