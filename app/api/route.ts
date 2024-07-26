963cbbe191be26bffeae4d7aeed52c4680322203
Chuf-lco <mainaantony07@gmail.com>
2024-07-26 17:39:17 +0300

    reading headers by calling the server function direclty in a Route Handler

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,2 +1,13 @@
+import { cookies } from 'next/headers'
+import { headers } from 'next/headers'
+
 export const dynamic = 'force-dynamic' // defaults to auto
-export async function GET(request: Request) {}
+export async function GET(request: Request) {
+    const cookieStore = cookies()
+    const token = cookieStore.get('token')
+
+    return new Response('Hello, Next.js!', {
+        status: 200,
+        headers: { 'Set-Cookie': 'token=${token.value}'},
+    })
+}
